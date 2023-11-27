b67fec96684616b053dfdf00d2cad823d46e7662
Jesse Tayler <jtayler@oeinc.com>
2023-11-27 13:53:59 -0500

    centralizing the ta api into a service

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -26,104 +26,75 @@ class AccountsController < ApplicationController
         if @user_settings.wants_verified_identity
           # Do something when wants_verified_identity is true
           puts "Display public identity profile is enabled!"
+          tru_anon_service = TruAnonService.new(@account)
+          profile_data = tru_anon_service.get_profile
 
-          service_name = "cryptoniteventures"
-          private_key = "z24acdhznmlocpop4embib4fj3hrkeaqv3oadt3dykuvln4ghqka"
+          # Use the filter function
+          social_configurations = []
+          if @user_settings.display_your_public_identity_profile
+            Rails.logger.info("display_your_public_identity_profile : #{profile_data}")
 
-          profile_url = "https://truanon.com/api/get_profile?id=#{@account.username}&service=#{service_name}"
-          Rails.logger.info('URL confirmation link...' + profile_url)
-
-          # Create an HTTP object
-          uri = URI.parse(profile_url)
-          http = Net::HTTP.new(uri.host, uri.port)
-          http.use_ssl = true
-
-          # Create a request
-          request = Net::HTTP::Get.new(uri.request_uri)
-          request['Authorization'] = private_key
-
-          response = http.request(request)
-          profile_data = JSON.parse(response.body)
-
-          if profile_data['type'] == 'error' && profile_data['title'] == 'Member Unknown'
-            # Unknown so we are going to fetch a token and build a smart confirmation link
+            if @user_settings.display_public_social_properties
+              puts "display_public_social_properties."
+              social_configurations = filter_data_configurations(profile_data, '', 'social')
+            else
+              puts "else display_public_social_properties."
+              social_configurations = filter_data_configurations(profile_data, 'truanon', 'social')
+              #truanon_only = filter_data_configurations(profile_data, 'truanon', 'social')
+              #social_configurations = [truanon_only]
+            end
           else
-            Rails.logger.info('Member is found...')
-
-            # Use the filter function
-            social_configurations = []
-            if @user_settings.display_your_public_identity_profile
-              Rails.logger.info("display_your_public_identity_profile : #{profile_data}")
-
-              if @user_settings.display_public_social_properties
-                puts "display_public_social_properties."
-                social_configurations = filter_data_configurations(profile_data, '', 'social')
-              else
-                puts "else display_public_social_properties."
-                social_configurations = filter_data_configurations(profile_data, 'truanon', 'social')
-                #truanon_only = filter_data_configurations(profile_data, 'truanon', 'social')
-                #social_configurations = [truanon_only]
-              end
+            puts "else display_your_public_identity_profile."
+            if @user_settings.display_public_social_properties
+              puts "display_public_social_properties."
+              social_configurations = filter_data_configurations(profile_data, '', 'social')
+              social_configurations.reject! { |profile_data| profile_data['dataPointType'] == 'truanon' }
             else
-              puts "else display_your_public_identity_profile."
-              if @user_settings.display_public_social_properties
-                puts "display_public_social_properties."
-                social_configurations = filter_data_configurations(profile_data, '', 'social')
-                social_configurations.reject! { |profile_data| profile_data['dataPointType'] == 'truanon' }
-              else
-                puts "else display_public_social_properties."
-              end
+              puts "else display_public_social_properties."
             end
-            Rails.logger.info('social_configurations: ' + social_configurations.to_s)
-
-            if @user_settings.display_verified_contact_information
-              puts "display_verified_contact_information."
-              primary_configurations = filter_data_configurations(profile_data, '', 'primary')
-              additional_configurations = filter_data_configurations(profile_data, '', 'additional')
-            else
-              puts "else display_verified_contact_information."
-              primary_configurations = []
-              additional_configurations = []
-            end
-            Rails.logger.info('social_configurations: ' + social_configurations.to_s)
-
-            if @user_settings.display_personal_information
-              puts "display_personal_information."
-              personal_configurations = filter_data_configurations(profile_data, '', 'personal')
-              personal_configurations.reject! { |personal_configurations| personal_configurations['dataPointType'] == 'bio' }
-
-              if @user_settings.omit_age
-                personal_configurations.reject! { |personal_configurations| personal_configurations['dataPointType'] == 'birthday' }
-              end
-
-              if @user_settings.omit_location
-                personal_configurations.reject! { |personal_configurations| personal_configurations['dataPointType'] == 'location' }
-              end
-
-              if @user_settings.omit_pronouns
-                personal_configurations.reject! { |profile_data| profile_data['dataPointType'] == 'gender' }
-              end
-
-            else
-              puts "else display_personal_information."
-              personal_configurations = []
-            end
-            Rails.logger.info('social_configurations: ' + social_configurations.to_s)
-
-            personal_configurations.reject! { |profile_data| profile_data['dataPointType'] == 'bio' }
-            Rails.logger.info('social_configurations: ' + social_configurations.to_s)
-            Rails.logger.info('personal_configurations: ' + personal_configurations.to_s)
-            Rails.logger.info('primary_configurations: ' + primary_configurations.to_s)
-            Rails.logger.info('additional_configurations: ' + additional_configurations.to_s)
-            new_configurations = social_configurations + personal_configurations + primary_configurations + additional_configurations
-            Rails.logger.info('new_configurations: ' + new_configurations.to_s)
-            # Replace the dataConfigurations array in profile_data
-            profile_data['dataConfigurations'] = new_configurations
-            #puts new_configurations
-            Rails.logger.info('profile_data: ' + profile_data.to_s)
-            @profile_data = profile_data
-
           end
+
+          if @user_settings.display_verified_contact_information
+            puts "display_verified_contact_information."
+            primary_configurations = filter_data_configurations(profile_data, '', 'primary')
+            additional_configurations = filter_data_configurations(profile_data, '', 'additional')
+          else
+            puts "else display_verified_contact_information."
+            primary_configurations = []
+            additional_configurations = []
+          end
+
+          if @user_settings.display_personal_information
+            puts "display_personal_information."
+            personal_configurations = filter_data_configurations(profile_data, '', 'personal')
+            personal_configurations.reject! { |personal_configurations| personal_configurations['dataPointType'] == 'bio' }
+
+            if @user_settings.omit_age
+              personal_configurations.reject! { |personal_configurations| personal_configurations['dataPointType'] == 'birthday' }
+            end
+
+            if @user_settings.omit_location
+              personal_configurations.reject! { |personal_configurations| personal_configurations['dataPointType'] == 'location' }
+            end
+
+            if @user_settings.omit_pronouns
+              personal_configurations.reject! { |profile_data| profile_data['dataPointType'] == 'gender' }
+            end
+
+          else
+            puts "else display_personal_information."
+            personal_configurations = []
+          end
+
+          personal_configurations.reject! { |profile_data| profile_data['dataPointType'] == 'bio' }
+          new_configurations = social_configurations + personal_configurations + primary_configurations + additional_configurations
+          # Replace the dataConfigurations array in profile_data
+          profile_data['dataConfigurations'] = new_configurations
+          #puts new_configurations
+          Rails.logger.info('profile_data: ' + profile_data.to_s)
+          @profile_data = profile_data
+
+          
         else
           new_configurations = []
 
--- app/controllers/settings/verifications_controller.rb
+++ app/controllers/settings/verifications_controller.rb
@@ -6,47 +6,10 @@ class Settings::VerificationsController < Settings::BaseController
   def show
     @verified_links = @account.fields.select(&:verified?)
 
-    service_name = "cryptoniteventures"
-    private_key = "z24acdhznmlocpop4embib4fj3hrkeaqv3oadt3dykuvln4ghqka"
+    tru_anon_service = TruAnonService.new(@account)
+    tru_anon_service.verify_user
 
-    profile_url = "https://truanon.com/api/get_profile?id=#{@account.username}&service=#{service_name}"
-    Rails.logger.info('URL confirmation link...' + profile_url)
-
-    # Create an HTTP object
-    uri = URI.parse(profile_url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-
-    # Create a request
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request['Authorization'] = private_key
-
-    response = http.request(request)
-    profile_data = JSON.parse(response.body)
-
-    if profile_data['type'] == 'error' && profile_data['title'] == 'Member Unknown'
-      # Unknown so we are going to fetch a token and build a smart confirmation link
-      # <a href=@verify_url target=ta-popup width=480, height=820, top=327.5, left=530>
-      token_url = "https://truanon.com/api/get_token?id=#{@account.username}&service=#{service_name}"
-      # Create an HTTP object
-      uri = URI.parse(token_url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-
-      # Create a request
-      request = Net::HTTP::Get.new(uri.request_uri)
-      request['Authorization'] = private_key
-
-      response = http.request(request)
-      token_data = JSON.parse(response.body)
-      verify_token = token_data["id"]
-      @verify_url = "https://truanon.com/api/verifyProfile?id=#{@account.username}&service=#{service_name}&token=#{verify_token}"
-
-      # {"id":"j4kabvsnd27eq","type":"Proof","active":true,"name":"New Proof"}
-      Rails.logger.info('verify_url from verify ' + @verify_url)
-    else
-      Rails.logger.info('Member is found...')
-    end
+    @verify_url = tru_anon_service.get_verify_url
   end
 
   def update
